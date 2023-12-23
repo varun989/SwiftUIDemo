59e5a47d357fa61841e8ce6a7b5ebac156ad4fe7
Varun <varunkapoor989@gmail.com>
2023-12-22 20:04:53 -0500

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Varun Kapoor on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
