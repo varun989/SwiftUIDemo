186fa420dd896ccb922a0fe0ea3a95ee99c31ff9
Varun <varunkapoor989@gmail.com>
2023-12-22 20:16:35 -0500

    Added an enum TimerType

---
+++ SwiftUIDemo/TimerType.swift
@@ -0,0 +1,14 @@
+//
+//  TimerType.swift
+//  SwiftUIDemo
+//
+//  Created by Varun Kapoor on 22/12/23.
+//
+
+import Foundation
+
+enum TimerType: String {
+    case timerA = "Timer A"
+    case timerB = "Timer B"
+    case timerC = "Timer C"
+}
